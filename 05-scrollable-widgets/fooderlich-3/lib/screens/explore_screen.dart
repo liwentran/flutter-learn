13e566bee0e35ddd480ac8ec9ca9773571a3a260
Liwen Tran <ltran29@jhu.edu>
2022-01-14 09:42:19 -0500

    05: Added a scroll controller Reports specfied positions

--- 05-scrollable-widgets/fooderlich-3/lib/screens/explore_screen.dart
+++ 05-scrollable-widgets/fooderlich-3/lib/screens/explore_screen.dart
@@ -4,11 +4,48 @@ import '../components/components.dart';
 import '../models/models.dart';
 import '../api/mock_fooderlich_service.dart';
 
-class ExploreScreen extends StatelessWidget {
+class ExploreScreen extends StatefulWidget {
+  ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
   // Create MockFooderlichService for mock server responses
   final mockService = MockFooderlichService();
 
-  ExploreScreen({Key? key}) : super(key: key);
+  late ScrollController _controller;
+
+  // listens to scroll offsets
+  void _scrollListener() {
+    if (_controller.offset >= _controller.position.maxScrollExtent &&
+        !_controller.position.outOfRange) {
+      print('i am at the bottom');
+    }
+    if (_controller.offset <= _controller.position.minScrollExtent &&
+        !_controller.position.outOfRange) {
+      print('i am at the top');
+    }
+  }
+
+  // initializes scroll controller and adds a listener.
+  // every time the user scrolls, scrollListener() will get alled
+  @override
+  void initState() {
+    super.initState();
+    _controller = ScrollController();
+    _controller.addListener(_scrollListener);
+  }
+
+  // perm. removes the object and is state from the tree
+  // memory cleanup includes unsubscribing from streams, disposing animations/
+  // controllers.
+  @override
+  void dispose() {
+    _controller.removeListener(_scrollListener);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +58,7 @@ class ExploreScreen extends StatelessWidget {
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           return ListView(
+            controller: _controller,
             scrollDirection: Axis.vertical,
             children: [
               // snapshot.data returns ExploreData, extract data from it
