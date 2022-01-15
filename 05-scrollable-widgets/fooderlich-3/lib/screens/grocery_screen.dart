e78f4e5082e56e22f1bfb8c65030e2c93d1d9671
Liwen Tran <ltran29@jhu.edu>
2022-01-15 11:38:43 -0500

    06: Created grocery screen

---
+++ 05-scrollable-widgets/fooderlich-3/lib/screens/grocery_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'empty_grocery_screen.dart';
+
+class GroceryScreen extends StatelessWidget {
+  const GroceryScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext build) {
+    return const EmptyGroceryScreen();
+  }
+  // TODO: Add build GroceryScreen
+}
